4b779d13260a62f99d2351c37a425ff6506d8f12
Helen Du <helendu.614@gmail.com>
2018-01-09 23:16:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,12 @@
 def starts_with_a_vowel?(word)
-  word.scan([aeiouAEIOU]).length > 0
+  word.scan(/\b[aeiouAEIOU]\w/).length > 0 
+  #\b is boundary of words in string
+  #/ to show regex 
+  #w is to find
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b(un\w+ing)\b/).flatten
 end
 
 def words_five_letters_long(text)
