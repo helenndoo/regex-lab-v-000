9ca154411da021e935680c089ebcff1c25e18480
Helen Du <helendu.614@gmail.com>
2018-01-09 23:22:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,8 @@
 def starts_with_a_vowel?(word)
   word.scan(/\b[aeiouAEIOU]\w/).length > 0
-
+  #\b is boundary of words in string
+  #/ to show regex
+  #w is to find
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
